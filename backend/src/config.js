813f73c7763df26d5f62521bff4fa1fcdca7881d
Doodle Jumper <doodlejumpthenft@gmail.com>
2022-04-01 19:39:36 -0300

    final updates

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,10 +56,10 @@ const MINT_PRICE = 98.7; // Minting price per NFT. Rinkeby = ETH, Polygon = MATI
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-08T02:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-09T02:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-04-07T02:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-08T02:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 450; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x54e41e066b9a249BF98268C8ed919b25D3243362"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
